8c2045354926ed6b7c0426b7eb93d96b3012de21
netto <euclydes.young@bluetab.net>
2023-07-04 09:41:53 +0200

    fix mix.exs version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdCluster.MixProject do
   def project do
     [
       app: :td_cluster,
-      version: "0.1.0",
+      version: "5.10.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
