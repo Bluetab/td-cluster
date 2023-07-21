2fda3ce6145c7a7a8080a16fdf5fa3e5bf4fad75
Juan Francisco Huete Verdejo <jf.huete@bluetab.net>
2023-07-21 12:30:10 +0200

    feat: TD-5809 Remove custom strategy and unify topologies

---
+++ lib/td_cluster/application.ex
@@ -0,0 +1,38 @@
+defmodule TdCluster.Application do
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    current_topology = fetch_current_topology()
+    topologies =  fetch_topologies()
+    |> Keyword.filter(fn {key, _} -> key == String.to_atom(current_topology) end)
+
+    children = [
+      TdCluster.ProcessGroup,
+      {Cluster.Supervisor, [topologies, [name: TdCluster.ClusterSupervisor]]}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: TdCluster.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  defp fetch_current_topology, do: System.get_env("CLUSTER_TOPOLOGY", "local_epdm")
+  defp fetch_topologies, do: [
+    local_epdm: [ strategy: Elixir.Cluster.Strategy.LocalEpmd ],
+    k8s: [
+      strategy: Elixir.Cluster.Strategy.Kubernetes,
+      config: [
+        mode: :ip,
+        kubernetes_node_basename: System.get_env("K8S_NODE_BASENAME", "truedat"),
+        kubernetes_selector: System.get_env("K8S_SELECTOR", ""),
+        kubernetes_namespace: System.get_env("K8S_NAMESPACE", "default"),
+        polling_interval: 10_000
+      ]
+    ],
+    docker: [ strategy: Elixir.Cluster.Strategy.Gossip ]
+  ]
+end
