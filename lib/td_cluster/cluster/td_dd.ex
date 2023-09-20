6de3898b31f7c59b0a9c3ded46ef157b89cdbaa4
netto <euclydes.young@bluetab.net>
2023-09-20 09:51:56 +0200

    feat: added getStructures function

--- lib/td_cluster/cluster/td_dd.ex
+++ lib/td_cluster/cluster/td_dd.ex
@@ -5,5 +5,9 @@ defmodule TdCluster.Cluster.TdDd do
     call_dd(TdDd.ReferenceData, :get, [id])
   end
 
+  def get_latest_structure_version(id) do
+    call_dd(TdDd.DataStructures, :get_latest_version, [id, [:data_fields]])
+  end
+
   defp call_dd(module, function, args), do: ClusterHandler.call(:dd, module, function, args)
 end
--- lib/td_cluster/test_helpers/td_dd_mock.ex
+++ lib/td_cluster/test_helpers/td_dd_mock.ex
@@ -5,4 +5,14 @@ defmodule TdCluster.TestHelpers.TdDdMock do
       expected
     end)
   end
+
+  def get_latest_structure_version(expect, id, expected) do
+    expect.(MockClusterHandler, :call, 1, fn :dd,
+                                             TdDd.DataStructures,
+                                             :get_latest_version,
+                                             [arg, [:data_fields]]
+                                             when arg == id ->
+      expected
+    end)
+  end
 end
