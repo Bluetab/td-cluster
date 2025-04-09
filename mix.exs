e246050eb6c88d5413b8ee8105ea9f341447a542
saul.isern <saul.isern@bluetab.net>
2025-04-09 10:41:59 +0200

    chore: update license

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdCluster.MixProject do
   def project do
     [
       app: :td_cluster,
-      version: "7.3.0",
+      version: "7.4.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
