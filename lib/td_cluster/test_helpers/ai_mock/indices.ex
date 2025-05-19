c4aae113ac1d65605ecbfe845d3eba9f1af68d52
saul.isern <saul.isern@bluetab.net>
2025-05-19 15:56:17 +0200

    fix: list indices mock

--- lib/td_cluster/test_helpers/ai_mock/indices.ex
+++ lib/td_cluster/test_helpers/ai_mock/indices.ex
@@ -3,11 +3,8 @@ defmodule TdCluster.TestHelpers.TdAiMock.Indices do
   Mocks Clusters for tests
   """
 
-  def list(expect, args, expected, times \\ 1) do
-    expect.(MockClusterHandler, :call, times, fn :ai,
-                                                 TdAi.Indices,
-                                                 :available_resource_mapping,
-                                                 [^args] ->
+  def list_indices(expect, args, expected, times \\ 1) do
+    expect.(MockClusterHandler, :call, times, fn :ai, TdAi.Indices, :list_indices, [^args] ->
       expected
     end)
   end
