d64ae04c37676f9c4a7811296f7d28e1b5d2a188
Guillermo Rodriguez <guillermo.r.navarrete@bluetab.net>
2025-10-22 11:56:21 +0200

    feat: TD-7302 add args to td-ai functions from index_type

--- lib/td_cluster/cluster/td_ai/indices.ex
+++ lib/td_cluster/cluster/td_ai/indices.ex
@@ -9,11 +9,11 @@ defmodule TdCluster.Cluster.TdAi.Indices do
     Cluster.TdAi.call_ai(TdAi.Indices, :list_indices, [args])
   end
 
-  def exists_enabled? do
-    Cluster.TdAi.call_ai(TdAi.Indices, :exists_enabled?, [])
+  def exists_enabled?(args \\ %{})  do
+    Cluster.TdAi.call_ai(TdAi.Indices, :exists_enabled?, [args])
   end
 
-  def first_enabled do
-    Cluster.TdAi.call_ai(TdAi.Indices, :first_enabled, [])
+  def first_enabled(args \\ %{})  do
+    Cluster.TdAi.call_ai(TdAi.Indices, :first_enabled, [args])
   end
 end
--- lib/td_cluster/test_helpers/ai_mock/indices.ex
+++ lib/td_cluster/test_helpers/ai_mock/indices.ex
@@ -9,14 +9,14 @@ defmodule TdCluster.TestHelpers.TdAiMock.Indices do
     end)
   end
 
-  def exists_enabled?(expect, expected, times \\ 1) do
-    expect.(MockClusterHandler, :call, times, fn :ai, TdAi.Indices, :exists_enabled?, [] ->
+  def exists_enabled?(expect, args, expected, times \\ 1) do
+    expect.(MockClusterHandler, :call, times, fn :ai, TdAi.Indices, :exists_enabled?, [^args] ->
       expected
     end)
   end
 
-  def first_enabled(expect, expected, times \\ 1) do
-    expect.(MockClusterHandler, :call, times, fn :ai, TdAi.Indices, :first_enabled, [] ->
+  def first_enabled(expect, args, expected, times \\ 1) do
+    expect.(MockClusterHandler, :call, times, fn :ai, TdAi.Indices, :first_enabled, [^args] ->
       expected
     end)
   end
