995f2c968f49202820fe6e2a463c916505a25021
Guillermo Rodriguez <guillermo.r.navarrete@bluetab.net>
2025-10-23 12:36:20 +0200

    refactor: TD-7302 add index_type parameter to generate_vector functions across modules

--- lib/td_cluster/cluster/td_ai/embeddings.ex
+++ lib/td_cluster/cluster/td_ai/embeddings.ex
@@ -5,11 +5,11 @@ defmodule TdCluster.Cluster.TdAi.Embeddings do
 
   alias TdCluster.Cluster
 
-  def all(text) do
-    Cluster.TdAi.call_ai(TdAi.Embeddings, :all, [text])
+  def all(text, index_type) do
+    Cluster.TdAi.call_ai(TdAi.Embeddings, :all, [text, index_type])
   end
 
-  def generate_vector(text, collection_name \\ nil) do
-    Cluster.TdAi.call_ai(TdAi.Embeddings, :generate_vector, [text, collection_name])
+  def generate_vector(text, index_type, collection_name \\ nil) do
+    Cluster.TdAi.call_ai(TdAi.Embeddings, :generate_vector, [text, index_type, collection_name])
   end
 end
