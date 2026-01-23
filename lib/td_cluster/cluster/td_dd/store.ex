81daf244737d6a49ec4c7971446aa71968f32134
saul.isern <saul.isern@bluetab.net>
2026-01-23 09:38:39 +0100

    fix: alias issue

--- lib/td_cluster/cluster/td_dd/store.ex
+++ lib/td_cluster/cluster/td_dd/store.ex
@@ -3,10 +3,12 @@ defmodule TdCluster.TdDd.Store do
   Module to interact with the store of the bg cluster
   """
 
-  alias TdCluster.Cluster.TdDd
+  alias TdCluster.Cluster
 
   def fetch(ids) do
-    {:ok, %{schema: schema, store: store}} = TdDd.call_dd(TdDd.DataStructures.Search, :store, [])
-    TdDd.call_dd(store, :fetch, [schema, ids])
+    {:ok, %{schema: schema, store: store}} =
+      Cluster.TdDd.call_dd(TdDd.DataStructures.Search, :store, [])
+
+    Cluster.TdDd.call_dd(store, :fetch, [schema, ids])
   end
 end
