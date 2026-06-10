c647b0cbec2e964a7084a732a28ecf157553f799
saul.isern <saul.isern@bluetab.net>
2026-06-10 11:32:16 +0200

    chore: TD-8168 update quality workflow

--- mix.exs
+++ mix.exs
@@ -4,10 +4,9 @@ defmodule TdCluster.MixProject do
   def project do
     [
       app: :td_cluster,
-      version: "8.3.0",
+      version: "8.7.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      aliases: aliases(),
       deps: deps()
     ]
   end
@@ -25,12 +24,6 @@ defmodule TdCluster.MixProject do
     [
       {:credo, "~> 1.7.11", only: [:dev, :test], runtime: false},
       {:libcluster, "~> 3.4.1"}
-    ]
-  end
-
-  defp aliases do
-    [
-      quality: ["format --check-formatted", "credo --strict"]
     ]
   end
 end
