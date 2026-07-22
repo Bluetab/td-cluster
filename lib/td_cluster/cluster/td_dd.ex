ac9a11c66f0b866245cc9d65bed8ddb4cc586e53
maitane.portilla <maitane.portilla@bluetab.net>
2026-07-22 15:02:59 +0200

    fix: TD-7599 update get_implementation_version_ids_by_ref

--- lib/td_cluster/cluster/td_dd.ex
+++ lib/td_cluster/cluster/td_dd.ex
@@ -26,7 +26,7 @@ defmodule TdCluster.Cluster.TdDd do
   end
 
   def get_implementation_version_ids_by_ref(implementation_ref) do
-    call_dd(TdDd.Implementations, :get_version_ids, [%{implementation_ref: implementation_ref}])
+    call_dd(TdDd.Implementations, :get_version_ids, [implementation_ref])
   end
 
   def agent_layer_call(module, function, args) when is_atom(module) do
